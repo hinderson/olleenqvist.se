2cf910d4a622cb5cc273bb6e1376b06ec42b2c46
hinderson <mattias@hinderson.com>
2015-12-01 19:35:12 +0100

    Adding myself as a user

---
+++ site/accounts/hinderson.php
@@ -0,0 +1,8 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: hinderson
+email: mattias@hinderson.com
+password: >
+  $2a$10$Zv5gVhKyMVlptjD4UmQkxOLvtWMdI6gcwd1O5ub051uvGC46T02Pe
+language: en
+role: admin
