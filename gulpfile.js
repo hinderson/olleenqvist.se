9a53c6729a068cdeffed7e131cf00c8d06eac435
hinderson <mattias@hinderson.com>
2015-12-01 19:59:24 +0100

    Adding gulp + dependencies

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var webpack = require('webpack');
+var watch = require('gulp-watch');
+var postcss = require('gulp-postcss');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('autoprefixer');
+var postcssImport = require('postcss-import');
+var csswring = require('csswring');
+var webpackConfig = require('./webpack.config.js');
+var imagemin = require('gulp-imagemin');
+var imageminJpegtran = require('imagemin-jpegtran');
+
+gulp.task('css', function ( ) {
+    return gulp.src('./assets/css/*.css')
+        .pipe(sourcemaps.init())
+        .pipe(postcss([
+            postcssImport(),
+            autoprefixer({ browsers: ['> 5%'] }),
+            csswring
+        ]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('optimize-thumbs', function ( ) {
+    return gulp.src('./thumbs/*.jpg')
+        .pipe(imageminJpegtran({progressive: true})())
+        .pipe(gulp.dest('./thumbs'));
+});
