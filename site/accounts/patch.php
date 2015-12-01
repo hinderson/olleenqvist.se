fdea34cbfa9b082979f199b42f1e0d8d975ce157
Patrick Hofweber <patchh@gmail.com>
2015-12-01 19:34:32 +0100

    adding myself as user

---
+++ site/accounts/patch.php
@@ -0,0 +1,8 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: patch
+email: patch@hofweber.se
+password: >
+  $2a$10$yPpoOhshwdOv0dlWKSZvw.lZpqvPHM7Y0PWi5.1sJKqDmnJIXkGey
+language: en
+role: admin
