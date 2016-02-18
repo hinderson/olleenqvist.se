907f310b75f1ac62fd0dcb770872ae118a58abc0
Mattias Hinderson <mattias@hinderson.com>
2016-02-18 21:53:35 +0100

    Flickity!

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,8 @@
 'use strict';
 
+// Requires
+var Flickity = require('flickity-imagesloaded');
+
 var viewToggler = document.querySelector('.view-toggler');
 viewToggler.addEventListener('click', function (e) {
     alert('Ayooooo');
