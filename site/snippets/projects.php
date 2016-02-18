2c55faffd8f320655147863f9b62e23da73fb364
Mattias Hinderson <mattias@hinderson.com>
2016-02-18 21:53:18 +0100

    Automagically crop thumbs in the correct aspect ratio

--- site/snippets/projects.php
+++ site/snippets/projects.php
@@ -1,6 +1,9 @@
 <h2 class="visuallyhidden">Latest projects</h2>
 
-<div class="projects">
+<section class="projects">
+    <header>
+        <h2 class="visuallyhidden">Projects</h2>
+    </header>
     <div class="group">
         <div class="group-inner">
             <?php foreach(page('projects')->children()->visible() as $project): ?>
@@ -11,11 +14,11 @@
                                 <a href="<?php echo $image->url() ?>">
                                     <?php
                                         $thumbGigantic = thumb($image, array('width' => 1500, 'quality' => 100));
-                                        $thumbLarge = thumb($image, array('width' => 980, 'quality' => 100));
-                                        $thumbMedium = thumb($image, array('width' => 860, 'quality' => 90));
-                                        $thumbSmall = thumb($image, array('width' => 440, 'quality' => 85));
+                                        $thumbLarge = thumb($image, array('width' => 980, 'height' => 653, 'crop' => true, 'quality' => 100));
+                                        $thumbMedium = thumb($image, array('width' => 860, 'height' => 573, 'crop' => true, 'quality' => 90));
+                                        $thumbSmall = thumb($image, array('width' => 440, 'height' => 293, 'crop' => true, 'quality' => 85));
                                     ?>
-                                    <img src="<?php echo $thumbMedium->url() ?>" srcset="<?php echo $thumbLarge->url() ?> <?php echo $thumbLarge->width() ?>w, <?php echo $thumbMedium->url() ?> <?php echo $thumbMedium->width() ?>w, <?php echo $thumbSmall->url() ?> <?php echo $thumbSmall->width() ?>w" sizes="100vw" alt="<?php echo $project->title()->html() ?>">
+                                    <img src="<?php echo $thumbSmall->url() ?>" srcset="<?php echo $thumbLarge->url() ?> <?php echo $thumbLarge->width() ?>w, <?php echo $thumbMedium->url() ?> <?php echo $thumbMedium->width() ?>w, <?php echo $thumbSmall->url() ?> <?php echo $thumbSmall->width() ?>w" sizes="100vw" alt="<?php echo $project->title()->html() ?>">
                                 </a>
                             </li>
                         <?php endforeach ?>
@@ -29,4 +32,4 @@
             <?php endforeach ?>
         </div>
     </div>
-</div>
+</section>
