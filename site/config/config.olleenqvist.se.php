3b10cc491795105acba2c55b13f537d9bd2fc228
Mattias Hinderson <mattias@hinderson.com>
2016-05-24 10:32:49 +0200

    Configuration

--- site/config/config.olleenqvist.se.php
+++ site/config/config.olleenqvist.se.php
@@ -8,9 +8,14 @@
     c::set('cache.driver', 'memcached');
 
     // Set global CDN url
-    c::set('cdnUrl', '');
+    c::set('cdnUrl', 'https://d11zb26c27eya9.cloudfront.net');
 
     // CDN settings
-    c::set('cdn.assets', '/dist');
-    c::set('cdn.content', '/content');
-    c::set('cdn.thumbs', '/thumbs');
+    c::set('cdn.assets', 'https://d11zb26c27eya9.cloudfront.net');
+    c::set('cdn.content', false);
+    c::set('cdn.thumbs', false);
+
+    // CDN paths
+    c::set('pathAssets', 'https://d11zb26c27eya9.cloudfront.net/dist');
+    c::set('pathThumbs', 'https://d11zb26c27eya9.cloudfront.net/thumbs');
+    c::set('pathContent', 'https://d11zb26c27eya9.cloudfront.net/content');
--- site/config/config.php
+++ site/config/config.php
@@ -34,3 +34,8 @@ c::set('debug', true);
 
 // Deactivate cache
 c::set('cache', false);
+
+// Path settings
+c::set('pathAssets', kirby()->urls()->assets());
+c::set('pathThumbs', kirby()->urls()->thumbs());
+c::set('pathContent', kirby()->urls()->content());
