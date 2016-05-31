6cc526c16028943d1344d7380b2a309eb806db4a
Mattias Hinderson <mattias@hinderson.com>
2016-05-31 16:13:29 +0200

    Tweak about overlay

--- site/snippets/about.php
+++ site/snippets/about.php
@@ -7,11 +7,23 @@
 
     <div class="content">
         <?php
-            $thumbSmall = thumb($about->images()->first(), array('width' => 600, 'height' => 800, 'quality' => 100, 'crop' => true));
-            $thumbMedium = thumb($about->images()->first(), array('width' => 440, '', 'quality' => 95, 'crop' => true));
-            $thumbLarge = thumb($about->images()->first(), array('width' => 200, '', 'quality' => 90, 'crop' => true));
+            $file = $about->images()->first();
+
+            $thumbLarge = thumb($file, array('width' => 1200, '', 'quality' => 90));
+            $thumbMedium = thumb($file, array('width' => 840, '', 'quality' => 90));
+            $thumbSmall = thumb($file, array('width' => 620, 'quality' => 90));
+            $thumbMicro = thumb($file, array('width' => 50, 'quality' => 20));
+
+            $aspectRatio = ($thumbMicro->height() / $thumbMicro->width()) * 100;
         ?>
-        <img src="<?php echo $thumbSmall->url() ?>" srcset="<?php echo $thumbLarge->url() ?> <?php echo $thumbLarge->width() ?>w, <?php echo $thumbMedium->url() ?> <?php echo $thumbMedium->width() ?>w, <?php echo $thumbSmall->url() ?> <?php echo $thumbSmall->width() ?>w" sizes="100vw" alt="">
+            <div class="progressive-media" data-attributes='{ "src": "<?php echo $thumbSmall->url() ?>", "srcset": "<?php echo $thumbLarge->url() ?> <?php echo $thumbLarge->width() ?>w, <?php echo $thumbMedium->url() ?> <?php echo $thumbMedium->width() ?>w, <?php echo $thumbSmall->url() ?> <?php echo $thumbSmall->width() ?>w", "sizes": "100vw", "alt": "" }'>
+                <div class="aspect-ratio" style="padding-bottom: <?php echo ($thumbLarge->height() / $thumbLarge->width()) * 100 ?>%;"></div>
+                <img src="<?php echo $thumbMicro->url() ?>" crossorigin="anonymous" aria-hidden="true" class="placeholder" alt="">
+                <canvas width="<?php echo $thumbLarge->width() ?>" height="<?php echo $thumbLarge->height() ?>"></canvas>
+                <noscript>
+                    <img src="<?php echo $thumbSmall->url() ?>" srcset="<?php echo $thumbLarge->url() ?> <?php echo $thumbLarge->width() ?>w, <?php echo $thumbMedium->url() ?> <?php echo $thumbMedium->width() ?>w, <?php echo $thumbSmall->url() ?> <?php echo $thumbSmall->width() ?>w" sizes="100vw" alt="">
+                </noscript>
+            </div>
         <div class="info">
             <?php echo $about->text()->kirbytext() ?>
         </div>
