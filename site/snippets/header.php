6e2d16c1a8b1a51a5ca1fcc5b1625e8439a965b6
Mattias Hinderson <mattias@hinderson.com>
2016-07-19 13:31:46 +0200

    Use correct CDN path for favicons

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -7,8 +7,8 @@
     <title><?php echo $site->title()->html() ?><?php if (!$page->isHomePage()) { echo " | " . $page->title()->html(); } ?></title>
     <script>document.documentElement.className = document.documentElement.className.replace(/(^|\\\b)no-js(\b|$)/, 'js');if (!("ontouchstart" in document.documentElement)) {document.documentElement.className += " no-touch";}</script>
 
-    <link rel="icon" type="image/png" href="<?php echo $site->url() ?>/assets/images/favicon-32x32.png" sizes="32x32" />
-    <link rel="icon" type="image/png" href="<?php echo $site->url() ?>/assets/images/favicon-16x16.png" sizes="16x16" />
+    <link rel="icon" type="image/png" href="<?php echo c::get('pathAssets') ?>/images/favicon-32x32.png" sizes="32x32" />
+    <link rel="icon" type="image/png" href="<?php echo c::get('pathAssets') ?>/images/favicon-16x16.png" sizes="16x16" />
 
     <meta name="description" content="<?php echo $site->description()->html() ?>">
 
