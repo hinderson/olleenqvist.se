9b619ec533a3f27ba1584500638655bd9712aa26
Mattias Hinderson <mattias@hinderson.com>
2018-05-13 14:41:10 +0200

    Update staging config

--- site/config/config.staging.olleenqvist.se.php
+++ site/config/config.staging.olleenqvist.se.php
@@ -4,7 +4,7 @@
 c::set('debug', false);
 
 // Activate cache
-c::set('cache', true);
+c::set('cache', false);
 c::set('cache.driver', 'memcached');
 
 // Force SSL
@@ -14,11 +14,11 @@ c::set('ssl', true);
 c::set('cdnUrl', 'https://d3omkl1g7kevbp.cloudfront.net');
 
 // CDN settings
-c::set('cdn.assets', 'https://d32x2ylr3hfw2f.cloudfront.net');
+c::set('cdn.assets', 'https://d3omkl1g7kevbp.cloudfront.net');
 c::set('cdn.content', false);
 c::set('cdn.thumbs', false);
 
 // CDN paths
-c::set('pathAssets', 'https://d32x2ylr3hfw2f.cloudfront.net');
+c::set('pathAssets', 'https://d3omkl1g7kevbp.cloudfront.net');
 c::set('pathThumbs', 'https://d3omkl1g7kevbp.cloudfront.net/thumbs');
 c::set('pathContent', 'https://d3omkl1g7kevbp.cloudfront.net/content');
